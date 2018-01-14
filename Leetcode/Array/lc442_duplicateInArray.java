69d9a29daec16c4572841f4bf1a3f6adfaf79674
Jingyi Ding <dingjy94@gmail.com>
2018-01-14 14:47:34 -0500

    Create lc442_duplicateInArray.java

---
+++ Leetcode/Array/lc442_duplicateInArray.java
@@ -0,0 +1,18 @@
+/**
+* Given an array of integers, 1 ≤ a[i] ≤ n (n = size of array), some elements appear twice and others appear once.
+* Find all the elements that appear twice in this array.
+* do it without extra space and in O(n) runtime
+*/
+class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[Math.abs(nums[i]) - 1] < 0)
+                result.add(Math.abs(nums[i]));
+            else 
+                nums[Math.abs(nums[i]) - 1] = -nums[Math.abs(nums[i]) - 1];
+        }
+        
+        return result;
+    }
+}
