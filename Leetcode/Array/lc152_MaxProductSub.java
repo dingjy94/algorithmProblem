7d7cd850580cfbff9bc208902b07649ffaf3a129
Jingyi Ding <dingjy94@gmail.com>
2018-01-21 12:37:02 -0500

    Update lc152_MaxProductSub.java

--- Leetcode/Array/lc152_MaxProductSub.java
+++ Leetcode/Array/lc152_MaxProductSub.java
@@ -1,13 +1,16 @@
 /**
 * Find the contiguous subarray within an array (containing at least one number) 
 * which has the largest product.
+* Dynamic programmming? store max and min value;
+* Proudcut question should always store min and max because negative integer;
+* 
 */
 class Solution {
     public int maxProduct(int[] nums) {
         if (nums.length <= 0 || nums == null)
             return 0;
         int result = nums[0];
-        int max = nums[0], min = nums[0];
+        int max = nums[0], min = nums[0]; // initialize with n[0], since Integer.MIN_VALUE's product cause some trouble
         
         for (int i = 1; i < nums.length; i++) {
             int tmp = max;
