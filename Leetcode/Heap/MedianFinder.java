066e54616f9cb737fedc4c5ed4c35af2b5961699
Jingyi Ding <dingjy94@gmail.com>
2018-08-28 10:00:50 -0700

    Create MedianFinder.java

---
+++ Leetcode/Heap/MedianFinder.java
@@ -0,0 +1,45 @@
+/**
+ * lc295 Find Median from Data Stream
+ */
+ 
+class MedianFinder {
+
+    /** initialize your data structure here. */
+    private class maxHeap implements Comparator<Integer> {
+        @Override
+        public int compare (Integer x, Integer y) {
+            return y - x;
+        }
+    }
+    PriorityQueue<Integer> minHalf;
+    PriorityQueue<Integer> maxHalf;
+    
+    public MedianFinder() {
+        minHalf = new PriorityQueue<>(10, new maxHeap());
+        maxHalf = new PriorityQueue<>();
+    }
+    
+    public void addNum(int num) {
+        maxHalf.add(num);
+        minHalf.add(maxHalf.poll());
+        
+        if (maxHalf.size() < minHalf.size()) {
+            maxHalf.add(minHalf.poll());
+        }
+    }
+    
+    public double findMedian() {
+        if (minHalf.size() == maxHalf.size()) {
+            return (minHalf.peek() + maxHalf.peek()) / 2.0;
+        } else {
+            return maxHalf.peek();
+        }
+    }
+}
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder obj = new MedianFinder();
+ * obj.addNum(num);
+ * double param_2 = obj.findMedian();
+ */
