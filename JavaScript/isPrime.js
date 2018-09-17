ac670cb81382da1ca451b6e725361f9971e86049
Jingyi Ding <dingjy94@gmail.com>
2018-09-17 14:55:10 -0700

    Update isPrime.js

--- JavaScript/isPrime.js
+++ JavaScript/isPrime.js
@@ -1,3 +1,10 @@
+/* all primes are of the form 6k ± 1, with the exception of 2 and 3. 
+ * This is because all integers can be expressed as (6k + i) for some integer k and 
+ * for i = ?1, 0, 1, 2, 3, or 4; 2 divides (6k + 0), (6k + 2), (6k + 4); 
+ * and 3 divides (6k + 3). So a more efficient method is to test if n is divisible by 2 or 3, 
+ * then to check through all the numbers of form 6k ± 1
+ */
+
 function isPrime(num) {
   if (num <= 1) return false;
   if (num <= 3) return true;
