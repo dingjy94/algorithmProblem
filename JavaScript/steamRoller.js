cd6f183a5b835482de574415727d57caf0a4dd80
Jingyi Ding <dingjy94@gmail.com>
2018-09-17 16:28:32 -0700

    Create steamRoller.js

---
+++ JavaScript/steamRoller.js
@@ -0,0 +1,23 @@
+function steamrollArray(arr) {
+  // I'm a steamroller, baby
+  const flat = [].concat(...arr); // flat one level
+  return flat.some(Array.isArray) ? steamrollArray(flat) : flat;
+}
+
+steamrollArray([1, [2], [3, [[4]]]]);
+
+// convert to string
+function steamrollArray(arr) {
+  return arr.toString()
+    .replace(',,', ',')       // "1,2,,3" => "1,2,3"
+    .split(',')               // ['1','2','3']
+    .map(function(v) {
+      if (v == '[object Object]') { // bring back empty objects
+        return {};
+      } else if (isNaN(v)) {        // if not a number (string)
+        return v;
+      } else {
+        return parseInt(v);         // if a number in a string, convert it
+      }
+    });
+}
